b9a8de1a6a4e99902fa9d23a808d34de0582ed8b
Gilberto Osuna <gilberto.osuna@outlook.com>
2015-02-27 09:53:51 -0600

    Added final

--- src/main/java/com/designpatterns/nullpattern/NullPatternDemo.java
+++ src/main/java/com/designpatterns/nullpattern/NullPatternDemo.java
@@ -2,9 +2,16 @@ package com.designpatterns.nullpattern;
 
 public class NullPatternDemo {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(final String[] args) {
+        final AbstractCustomer customer1 = CustomerFactory.getCustomer("John");
+        final AbstractCustomer customer2 = CustomerFactory.getCustomer("Michael");
+        final AbstractCustomer customer3 = CustomerFactory.getCustomer("Julie");
 
+        System.out.println("Customers");
+        System.out.println(customer1.getName());
+        System.out.println(customer2.getName());
+        System.out.println(customer3.getName());
+ 
     }
 
 }
